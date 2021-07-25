f570d90f211ea292d95ff994055cf79412a75437
Hiroshi Siqueira <hiroshi.siq@gmail.com>
2021-07-25 18:50:38 -0300

    feat: change text to 'Welcome back!'

--- src/app/app.test.js
+++ src/app/app.test.js
@@ -6,8 +6,8 @@ describe("Test the / path", () => {
     const response1 = await request(app).get("/");
     const response2 = await request(app).get("/");
     
-    expect(response1.text).toBe('<h1>Welcome!</h1><p>This page was visited 1 time.</p>')
-    expect(response2.text).toBe('<h1>Welcome!</h1><p>This page was visited 2 times.</p>')
+    expect(response1.text).toBe('<h1>Welcome back!</h1><p>This page was visited 1 time.</p>')
+    expect(response2.text).toBe('<h1>Welcome back!</h1><p>This page was visited 2 times.</p>')
     expect(response1.statusCode).toBe(200);
     expect(response2.statusCode).toBe(200);
   });
