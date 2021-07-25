7b369be0d8d21132473b342ce455c773f740f5e8
Hiroshi Siqueira <hiroshi.siq@gmail.com>
2021-07-25 16:00:27 -0300

    test: add test for get /

---
+++ src/app/app.test.js
@@ -0,0 +1,14 @@
+const request = require("supertest");
+const app = require("./app");
+
+describe("Test the / path", () => {
+  test("It should accept GET request", async () => {
+    const response1 = await request(app).get("/");
+    const response2 = await request(app).get("/");
+    
+    expect(response1.text).toBe('<h1>Welcome!</h1><p>This page was visited 1 time.</p>')
+    expect(response2.text).toBe('<h1>Welcome!</h1><p>This page was visited 2 times.</p>')
+    expect(response1.statusCode).toBe(200);
+    expect(response2.statusCode).toBe(200);
+  });
+});
