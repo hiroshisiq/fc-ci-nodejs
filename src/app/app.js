cdd1d02c6eb7d9dd01f97c6b633c3a46ce241256
Hiroshi Siqueira <hiroshi.siq@gmail.com>
2021-07-25 15:31:50 -0300

    refactor: change folder hierarchy

--- src/app/app.js
+++ src/app/app.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const app = express()
-const port = 3000
 
 let counter = 0
 
@@ -9,6 +8,4 @@ app.get('/', (req, res) => {
   res.send(`<h1>Welcome!</h1><p>This page was visited ${counter} time${counter>1?'s':''}.</p>`)
 })
 
-app.listen(port, () => {
-  console.log(`App listening at http://localhost:${port}`)
-})
+module.exports = app;
